2f5a09cd055e1c4cf2ac1d0f933bade9c7425658
tiwxri <tiwxri@gmail.com>
2025-04-15 05:25:43 +0530

    s

--- services/paymentServices.js
+++
@@ -1,7 +0,0 @@
-async function processPayment(userId, amount) {
-    // Your Razorpay or Stripe logic here
-    return `🧾 Payment of ₹${amount} for user ${userId} processed.`;
-  }
-  
-  module.exports = { processPayment };
-  
--- services/whatsappBot.js
+++
@@ -1,3 +0,0 @@
-client.on('message', async (message) => {
-  console.log(`📩 New message received: ${message.body} from ${message.from}`);
-});
