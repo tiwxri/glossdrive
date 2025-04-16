c516414fa541efc474c3d5b5e2e948715ceb7c51
tiwxri <tiwxri@gmail.com>
2025-04-16 21:33:50 +0530

    refactored

--- firebase.js
+++ firebase.js
@@ -1,11 +1,13 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/glossdrivemvp-firebase-adminsdk-fbsvc-e7345fe6bf.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://glossdrivemvp-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  databaseURL: 'https://glossdrivemvp.firebaseio.com',
 });
 
-const db = admin.firestore(); // Or use .database() for Realtime DB
-
+const db = admin.firestore();
 module.exports = db;
