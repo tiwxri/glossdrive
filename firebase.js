293f458fd14c33d02e1e0f94f9a8238a6af109c3
tiwxri <tiwxri@gmail.com>
2025-04-16 11:59:10 +0530

    Added the DB URL

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./config/serviceAccountKey.json');
+const serviceAccount = require('./config/glossdrivemvp-firebase-adminsdk-fbsvc-e7345fe6bf.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://<your-project-id>.firebaseio.com"
+  databaseURL: "https://glossdrivemvp-default-rtdb.asia-southeast1.firebasedatabase.app/"
 });
 
 const db = admin.firestore(); // Or use .database() for Realtime DB
