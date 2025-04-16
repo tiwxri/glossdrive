9cc5ec006d967cbad1dd4edb5a2a29f6836a7e51
tiwxri <tiwxri@gmail.com>
2025-04-17 02:54:36 +0530

    added the private key

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const admin = require('firebase-admin');
-const serviceAccount = require(path.resolve(__dirname, 'serviceAccount.json'));
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
