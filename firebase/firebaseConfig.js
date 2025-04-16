d6c6b4880ecc6b0f81743c94077630c8aac83c21
tiwxri <tiwxri@gmail.com>
2025-04-17 03:15:37 +0530

    fixed the error logs

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,11 +1,16 @@
-const path = require('path');
 const admin = require('firebase-admin');
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://glossdrivemvp.firebaseio.com'
+  databaseURL: `https://glossdrivemvp.firebaseio.com`,
 });
 
 const db = admin.firestore();
+
 module.exports = { db };
