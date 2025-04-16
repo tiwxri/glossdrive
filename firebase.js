8a6d1040ce8c4c8a13236f7279d5f2e82a2c041a
tiwxri <tiwxri@gmail.com>
2025-04-16 11:45:54 +0530

    udpated the firebase code

--- firebase.js
+++ firebase.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./path/to/your/serviceAccountKey.json');
+const serviceAccount = require('./config/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
