51e6fc5bbe9e38d690b3ba6bf1bffafd9b9c707e
tiwxri <tiwxri@gmail.com>
2025-04-17 02:33:00 +0530

    minor changes to FirebaseConfig file

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json'); // from Firebase
+const serviceAccount = require(path.resolve(__dirname, 'serviceAccount.json'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
