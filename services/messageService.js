9ffa3d2403f31caff385f7457d202ef847350748
tiwxri <tiwxri@gmail.com>
2025-04-18 02:01:12 +0530

    fixing errors

--- services/messageService.js
+++ services/messageService.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
 
+console.log('📞 WHATSAPP_PHONE_ID:', process.env.WHATSAPP_PHONE_ID);
+console.log('🔐 WHATSAPP_TOKEN:', process.env.WHATSAPP_TOKEN ? 'SET' : 'NOT SET');
+
 exports.sendMessage = async (phone, message) => {
+  
   await axios.post(`https://graph.facebook.com/v19.0/${process.env.WHATSAPP_PHONE_ID}/messages`, {
     messaging_product: 'whatsapp',
     to: phone,
