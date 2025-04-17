9f1f509797554ec645c41fb82678a6f3d4b4709a
tiwxri <tiwxri@gmail.com>
2025-04-18 03:28:39 +0530

    wasn't reading the greeting text

--- routes/webhook.js
+++ routes/webhook.js
@@ -8,8 +8,10 @@ router.post('/', async (req, res) => {
 
     if (body.object && body.entry && body.entry[0].changes && body.entry[0].changes[0].value.messages) {
       const message = body.entry[0].changes[0].value.messages[0];
+      const sender = message.from;
 
-      let msg;
+      let msg = '';
+
       if (message.type === 'interactive') {
         const interactive = message.interactive;
         if (interactive.type === 'button_reply') {
@@ -18,11 +20,17 @@ router.post('/', async (req, res) => {
           msg = interactive.list_reply.id;
         }
       } else if (message.type === 'text') {
-        msg = message.text.body;
+        msg = message.text.body.trim().toLowerCase();
       }
 
-      if (msg) {
-        await chatbotController.handleIncomingMessage(message.from, msg);
+      // Handle greetings like "hi", "hello", "start"
+      const greetings = ['hi', 'hello', 'hey', 'start'];
+      if (greetings.includes(msg)) {
+        // You can start with the first flow step
+        const flowSteps = require('../utils/constants').flowSteps;
+        await chatbotController.sendMessage(sender, flowSteps.chooseService);
+      } else if (msg) {
+        await chatbotController.handleIncomingMessage(sender, msg);
       }
     }
 
