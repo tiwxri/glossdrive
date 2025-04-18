aad1022de1820489cd9d4dc575b1e3bdac711e62
tiwxri <tiwxri@gmail.com>
2025-04-18 23:20:37 +0530

    updated the logic to fix the crash

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 // Path to your service account JSON file
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG.replace(/\\n/g, '\n')); // ✅ make sure this file exists
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT.replace(/\\n/g, '\n')); // ✅ make sure this file exists
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
